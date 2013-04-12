93902768255fe204c78a44dbdf6a414064b8d925
sky <b.sky@163.com>
2013-04-12 14:58:45 +0800

    Initial Commit

---
+++ DPlibrary_UI/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DPlibrary_UI
+//
+//  Created by apple on 13-4-12.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DPlibrary_UI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DPlibrary_UI
+//
+//  Created by apple on 13-4-12.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
